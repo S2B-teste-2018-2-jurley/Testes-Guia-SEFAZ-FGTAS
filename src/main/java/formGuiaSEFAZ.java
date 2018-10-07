352dbdd93f7d74df24135b79e9b233de1faaf52e
Jurley-Ribeiro <jurley76@hotmail.com>
2018-10-07 20:54:12 -0300

    Commit realizado com sucesso

---
+++ src/main/java/formGuiaSEFAZ.java
@@ -0,0 +1,11 @@
+//package com.jurley.exemplo.selenium;
+
+    public class formGuiaSEFAZ {
+
+        /**
+         * Hello world!
+         */
+            public static void main(String[] args) {
+                System.out.println("Hello World!");
+            }
+        }
---
+++ src/test/java/FormGuiaSEFAZTest.java
@@ -0,0 +1,73 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+    /**
+     * @see https://github.com/s2b-teste-2018-2/webdrivermanager-examples/blob/master/src/test/java/io/github/bonigarcia/wdm/test/ChromeTest.java
+     * @author teste
+     *
+     */
+    public class FormGuiaSEFAZTest {
+
+        @Test
+        public void givenGooglePageWhenSearchingForWikipediaThenAWikipediaSearchBoxAppears() {
+            System.setProperty("webdriver.chrome.driver",
+                    "D:\\User\\Download\\Selenium\\chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://bit.ly/2IogiOY");
+
+            WebElement element = driver.findElement(By.xpath("//*[@id='painelConteudo']/table/tbody/tr[4]/td[2]/select/option[3]"));
+            element.click();
+            WebElement button = driver.findElement(By.className("botaoMed")); //Botão avançar
+            button.click();
+
+            WebElement formDtPagamento = driver.findElement(By.name("DtLimite"));
+            formDtPagamento.sendKeys("10102018");
+            formDtPagamento.click();
+
+            WebElement formCpf = driver.findElement(By.name("Identificacao"));
+            formCpf.sendKeys("68772148209");
+            formCpf.click();
+
+            WebElement formReferencia = driver.findElement(By.name("Referencia"));
+            formReferencia.sendKeys("02");
+            formReferencia.click();
+
+            WebElement formVencimento = driver.findElement(By.name("Vencimento"));
+            formVencimento.sendKeys("11112018");
+            formVencimento.click();
+
+            WebElement formNome = driver.findElement(By.name("NomeContribuinte"));
+            formNome.sendKeys("0joao Ninguem");
+            formNome.click();
+
+            WebElement button2 = driver.findElement(By.className("botaoMed")); //Botão avançar
+            button2.click();
+
+            WebElement pdf = driver.findElement(By.xpath("//*[@id='painelConteudo']/table/tbody/tr[15]/td/table/tbody/tr/td[2]/a"));
+            pdf.click();
+            //element.sendKeys("wikipedia");
+            //element.submit();
+
+            /*
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            WebElement messageElement = wait.until(
+                    ExpectedConditions.presenceOfElementLocated(By.id("nqsbq"))
+            );
+
+            assertNotNull(messageElement);
+
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            driver.close();
+            */
+        }
+}
